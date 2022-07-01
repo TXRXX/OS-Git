da945d1e03a02bfbc85499c246af4f1410fc0082
TXRXX <toro2544@gmail.com>
2022-07-01 14:54:55 +0700

    init lab1

---
+++ lab1.sh
@@ -0,0 +1,56 @@
+# 1.1
+-bash-4.1$ pwd
+/home/osusr05
+
+# 1.2
+-bash-4.1$ ls
+test
+
+# 1.3
+-bash-4.1$ ls -R /usr/include/
+
+# 1.4
+-bash-4.1$ cd ..
+-bash-4.1$ pwd
+/home
+
+# 1.5
+-bash-4.1$ cd osusr05
+-bash-4.1$ pwd
+/home/osusr05
+
+# 1.6
+-bash-4.1$ mkdir subtest2 subtest3
+-bash-4.1$ ls
+subtest2  subtest3
+
+# 1.7
+-bash-4.1$ cp /home/test1.txt test2.txt
+-bash-4.1$ ls
+test2.txt
+
+# 1.8
+-bash-4.1$ nano test2.txt 
+
+Hello World
+Operating System Laboratory
+
+# 1.9
+-bash-4.1$ rmdir subtest3
+-bash-4.1$ ls
+subtest2
+
+# 1.10
+-bash-4.1$ top
+
+# 1.11
+-bash-4.1$ who
+osusr05  pts/0        Jul  1 13:25 (10.48.108.24)
+osusr101 pts/1        Jul  1 13:52 (10.48.104.53)
+osusr24  pts/2        Jul  1 13:53 (10.224.109.235)
+osusr80  pts/3        Jul  1 12:01 (10.51.92.235)
+osusr78  pts/10       Jul  1 11:05 (10.224.108.133)
+osusr67  pts/11       Jul  1 11:05 (10.225.8.21)
+
+# 1.12
+# ทำไม่เป็น
